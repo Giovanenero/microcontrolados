b08b31d732ed8c2115a4bd91dc0e69ae37f0fb10
Rafael Bergo <rafaelbergo503@gmail.com>
2025-09-01 19:28:09 -0300

    lab 0

---
+++ 0/Projeto Exemplo/Projeto Exemplo/exemplo.s
@@ -0,0 +1,61 @@
+; Exemplo.s
+; Desenvolvido para a placa EK-TM4C1294XL
+; Prof. Guilherme Peron
+; 12/03/2018
+
+; -------------------------------------------------------------------------------
+        THUMB                        ; Instruções do tipo Thumb-2
+; -------------------------------------------------------------------------------
+; Declarações EQU - Defines
+;<NOME>         EQU <VALOR>
+; -------------------------------------------------------------------------------
+; Área de Dados - Declarações de variáveis
+		AREA  DATA, ALIGN=2
+		; Se alguma variável for chamada em outro arquivo
+		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a variável <var> a 
+		                                   ; partir de outro arquivo
+;<var>	SPACE <tam>                        ; Declara uma variável de nome <var>
+                                           ; de <tam> bytes a partir da primeira 
+                                           ; posição da RAM		
+
+; -------------------------------------------------------------------------------
+; Área de Código - Tudo abaixo da diretiva a seguir será armazenado na memória de 
+;                  código
+        AREA    |.text|, CODE, READONLY, ALIGN=2
+
+		; Se alguma função do arquivo for chamada em outro arquivo	
+        EXPORT Start                ; Permite chamar a função Start a partir de 
+			                        ; outro arquivo. No caso startup.s
+									
+		; Se chamar alguma função externa	
+        ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
+									; função <func>
+
+; -------------------------------------------------------------------------------
+; Função main()
+
+memInicio EQU 0x20000400
+memFim EQU 0x20000600
+
+Start  
+; Comece o código aqui <======================================================
+
+	LDR R10, =memInicio
+	LDR R11, =memFim
+	;LDR R12, #0 ; i que percorre a memoria
+	
+percorre
+	;LDRH R10, [R11 + R12] 
+	;LDRH R10, [R11, R12 +2] 
+	
+	CMP R1, R2
+	
+troca
+
+
+
+ordena
+
+	NOP
+    ALIGN                           ; garante que o fim da seção está alinhada 
+    END                             ; fim do arquivo

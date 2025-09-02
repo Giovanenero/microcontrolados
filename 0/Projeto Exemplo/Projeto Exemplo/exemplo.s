dfcddf19637bf5acb8ea5bee9de6c8c10a3d087f
Giovanenero <giovanelimas16@hotmail.com>
2025-09-02 01:36:09 -0300

    finalizando bubblesorte

--- 0/Projeto Exemplo/Projeto Exemplo/exemplo.s
+++ 0/Projeto Exemplo/Projeto Exemplo/exemplo.s
@@ -40,21 +40,137 @@ memFim EQU 0x20000600
 Start  
 ; Comece o código aqui <======================================================
 
-	LDR R10, =memInicio
-	LDR R11, =memFim
-	;LDR R12, #0 ; i que percorre a memoria
+	LDR R10, =memInicio	; ponteiro do loop externo
+	LDR R12, =memFim
+	LDRH R2, [R12]
+	SUB R10, R10, #2
+	B loopExterno
+
+atualiza
+	LDR R4, [R10]
+	LDR R5, [R11]
+	STRH R4, [R11]
+	STRH R5, [R10]
+	B loopInternoNext	
 	
-percorre
-	;LDRH R10, [R11 + R12] 
-	;LDRH R10, [R11, R12 +2] 
+loopExterno
+	ADD R10, R10, #2
+	LDRH R0, [R10]
+	CMP R0, R2
+	BEQ fim
+
+	ADD R11, R10, #2
 	
+loopInterno
+	LDRH R1, [R11]
 	CMP R1, R2
+	BEQ loopExterno
+
+	LDRH R0, [R10]
+	LDRH R1, [R11]
+	CMP R0, R1
+	BGT atualiza
+
+loopInternoNext
+	ADD R11, R11, #2
+	B loopInterno
+
+
+
+; ============================================
+
+fim
+
+	LDR R10, =memInicio   ; ponteiro para o início do vetor
+    MOV R6, #0            ; maior tamanho de PG encontrado
+    LDRH R7, [R10]           ; primeiro elemento da maior PG
+    MOV R14, #0           ; razão da maior PG
 	
-troca
+loop_inicio
+	LDRH R0, [R10]
+    CMP R0, #0
+    BGE fimPrograma      ; fim do vetor
+    MOV R11, R10          ; ponteiro para o próximo elemento
+   
+proximoDivisor
+	ADD R11, R11, #2
+	LDRH R1, [R11]
+    CMP R1, #0
+	BGE testaPG      ; não há próximo elemento
+    B encontraDivisores
+	
+encontraDivisores
+	; R10 = elemento atual (a[i])
+    ; R11 = elemento seguinte (a[i+1])
+        
+    LDRH R0, [R11]        ; b = a[i+1]
+    LDRH R1, [R10]        ; a = a[i]
+    UDIV R3, R0, R1       ; r = b / a
+    MLS  R4, R3, R1, R0   ; resto = b - r*a
+    CMP  R4, #0
+    BNE proximoDivisor    ; se resto != 0, não é divisor válido
+
+    ; temos razão válida
+    PUSH {R3}             ; salva razão na pilha
+    B proximoDivisor
+
+
+testaPG
+	LDRH R0, [R10]
+	CMP R0, #0
+	BEQ fimPrograma
+	
+	POP {R9}
+	
+loopTestaPG
+
+	MUL R0, R7, R9		; a2 = a1 * q
+	UDIV R1, R1,         ; atual / anterior
+    MLS  R4, R9, R1, R0   ; resto
+	CMP  R4, #0
+	
 
 
 
-ordena
+testaPg
+	POP {R3}              ; pega a razão candidata
+    MOV R8, #2            ; contador da sequência (2 elementos já considerados)
+	MOV R12, R10          ; ponteiro para o início da sequência
+	ADD R13, R12, #2      		; próximo elemento a verificar
+	
+loopPg
+	CMP R13, R12          ; verifica limite do vetor
+    BGE verificaMaior
+	CMP R13, R12
+    BGE verificaMaior
+
+	LDRH R0, [R13]        ; atual
+    LDRH R1, [R12]        ; anterior
+	UDIV R9, R0, R1       ; atual / anterior
+    MLS  R4, R9, R1, R0   ; resto
+	CMP  R4, #0
+	BNE verificaMaior
+	CMP  R9, R3            ; quociente == razão?
+	BNE verificaMaior
+
+	ADD R8, R8, #1
+    ADD R12, R12, #2
+	ADD R13, R13, #2
+	B loopPg
+	
+verificaMaior
+	CMP R8, R6
+    BLE continuaLoop
+	MOV R6, R8            ; atualiza tamanho da maior sequência
+	LDRH R7, [R10]        ; salva primeiro elemento
+	MOV R14, R3           ; salva a razão
+	
+continua_loop
+	ADD R10, R10, #2      ; avança para o próximo elemento do vetor
+    CMP R10, R12
+	BLT loopInicio
+	
+fimPrograma
 
 	NOP
     ALIGN                           ; garante que o fim da seção está alinhada 

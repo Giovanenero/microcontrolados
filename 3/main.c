e4b63a2accd22c6a5427a51a085f8000100f49b8
Rafael Bergo <rafaelbergo503@gmail.com>
2025-11-14 19:37:19 -0300

    lab 3 add more states and terminal messages

--- 3/main.c
+++ 3/main.c
@@ -22,6 +22,9 @@ typedef enum estMotor
 {
 	INICIAL,
 	MODO_CONTROLE,
+	MODO_SENTIDO,
+	MODO_VELOCIDADE,
+	MOTOR_LIGADO,
 	CONTROLE_POTENCIOMETRO,
 	CONTROLE_TERMINAL,
 } estadosMotor;
@@ -29,7 +32,8 @@ typedef enum estMotor
 typedef struct motor
 {
 	estadosMotor estado;
-	uint8_t* mensagem;
+	uint8_t modo; // 0 = terminal, 1 = potenciometro
+	uint8_t sentido; // 0 = horario, 1 = anti-horario
 	uint8_t velocidade;
 } motorTipo;
 
@@ -46,6 +50,9 @@ void ImprimeFraseUART(uint8_t* texto);
 void SetEstado(void);
 void ClearUART(void);
 uint8_t INICIAL_MSG[] = "Motor parado, pressione * para iniciar";
+uint8_t MODO_CONTROLE_MSG[] = "Defina o modo de controle do motor: p = potenciometro, t = terminal";
+uint8_t MODO_SENTIDO_MSG[] = "Defina o sentido de rotacao: h = horario, a = anti-horario";
+uint8_t MODO_VELOCIDADE_MSG[] = "Defina a velocidade do motor: 5 = 50%, 6 = 60%, 7 = 70%, 8 = 80%, 9 = 90%, 0 = 100%";
 
 
 uint8_t Flag = 0;
@@ -74,15 +81,47 @@ int main(void)
 				break;
 				
 			case MODO_CONTROLE:
-				ImprimeFraseUART(INICIAL_MSG);
+				if(Flag == 0)
+				{
+					ImprimeFraseUART(MODO_CONTROLE_MSG);
+					Flag = 1;
+				}
+				SetEstado();
+				break;
+			
+			case CONTROLE_TERMINAL:
+				if(Flag == 0)
+				{
+					//ImprimeFraseUART();
+					Flag = 1;
+				}
+				SetEstado();
+				break;
+			
+			case MODO_SENTIDO:
+				if(Flag == 0)
+				{
+					ImprimeFraseUART(MODO_SENTIDO_MSG);
+					Flag = 1;
+				}
+				SetEstado();
+				break;
+			
+			case MODO_VELOCIDADE:
+				if(Flag == 0)
+				{
+					ImprimeFraseUART(MODO_VELOCIDADE_MSG);
+					Flag = 1;
+				}
+				SetEstado();
 				break;
 			
 			case CONTROLE_POTENCIOMETRO:
 				ImprimeFraseUART(INICIAL_MSG);
 				break;
 			
-			case CONTROLE_TERMINAL:
-				ImprimeFraseUART(INICIAL_MSG);
+			case MOTOR_LIGADO:
+				//LigaMotor
 				break;
 		}
 	}
@@ -147,13 +186,134 @@ void SetEstado()
 		if(value == '*')
 		{
 			motor.estado = MODO_CONTROLE;
-			//SetUART('2');
+			motor.velocidade = 0;
+			Flag = 0;
 		}
 	}
+	
 	else if(motor.estado == MODO_CONTROLE)
 	{
-	
+		if(value == 't') // modo terminal (motor.modo = 0)
+		{
+			motor.modo = 0;
+			motor.velocidade = 0;
+			motor.estado = MODO_SENTIDO;
+			Flag = 0;
+		}
+		else if(value == 'p') // modo potenciometro (motor.modo = 1)
+		{
+			motor.modo = 1;
+			motor.velocidade = 0;
+			//motor.estado = MODO_SENTIDO;
+			Flag = 0;
+		}
 	}
+	
+	else if(motor.estado == MODO_SENTIDO)
+	{
+		if(value == 'h') // sentido horario (motor.sentido = 0)
+		{
+			motor.sentido = 0;
+			motor.velocidade = 0;
+			motor.estado = MODO_VELOCIDADE;
+			Flag = 0;
+		}
+		else if(value == 'a') // sentido anti-horario (motor.sentido = 1)
+		{
+			motor.sentido = 1;
+			motor.velocidade = 0;
+			motor.estado = MODO_VELOCIDADE;
+			Flag = 0;
+		}
+	}
+	
+	else if(motor.estado == MODO_VELOCIDADE)
+	{
+		if(value == '5') // velocidade = 50%
+		{
+			motor.velocidade = 50;
+			motor.estado = MOTOR_LIGADO;
+			Flag = 0;
+		}
+		else if(value == '6') // velocidade = 60%
+		{
+			motor.velocidade = 60;
+			motor.estado = MOTOR_LIGADO;
+			Flag = 0;
+		}
+		else if(value == '7') // velocidade = 70%
+		{
+			motor.velocidade = 60;
+			motor.estado = MOTOR_LIGADO;
+			Flag = 0;
+		}
+		else if(value == '8') // velocidade = 80%
+		{
+			motor.velocidade = 60;
+			motor.estado = MOTOR_LIGADO;
+			Flag = 0;
+		}
+		else if(value == '9') // velocidade = 90%
+		{
+			motor.velocidade = 60;
+			motor.estado = MOTOR_LIGADO;
+			Flag = 0;
+		}
+		else if(value == '0') // velocidade = 100%
+		{
+			motor.velocidade = 60;
+			motor.estado = MOTOR_LIGADO;
+			Flag = 0;
+		}
+	}
+	
+	else if(motor.estado == MOTOR_LIGADO)
+	{
+		// Altera o sentido
+		if(value == 'h') // sentido horario (motor.sentido = 0)
+		{
+			motor.sentido = 0;
+		}
+		else if(value == 'a') // sentido anti-horario (motor.sentido = 1)
+		{
+			motor.sentido = 1;
+		}
+		
+		//Altera a velocidade
+		if(value == '5') // velocidade = 50%
+		{
+			motor.velocidade = 50;
+		}
+		else if(value == '6') // velocidade = 60%
+		{
+			motor.velocidade = 60;
+		}
+		else if(value == '7') // velocidade = 70%
+		{
+			motor.velocidade = 60;
+		}
+		else if(value == '8') // velocidade = 80%
+		{
+			motor.velocidade = 60;
+		}
+		else if(value == '9') // velocidade = 90%
+		{
+			motor.velocidade = 60;
+		}
+		else if(value == '0') // velocidade = 100%
+		{
+			motor.velocidade = 60;
+		}
+		
+		//Para o motor
+		else if(value == 's')
+		{
+			motor.velocidade = 0;
+			motor.estado = INICIAL;
+			Flag = 0;
+		}
+	}
+	
 }
 	
 void AcendeLeds(int8_t led)
